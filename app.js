f15dbe1f0a485f04593e52772b37a7a2b539a095
CandiolliMateus <mateuscandiolli.1909@gmail.com>
2023-08-19 23:02:05 -0300

    fazendo um git restore

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está crescendo")
-
-console.log("Modificação 01")
-console.log("Modificação 02")
-console.log("Modificação 03")
-
-
-console.log("Modificação 04")
-console.log("Modificação 05")
-console.log("Modificação 06")
